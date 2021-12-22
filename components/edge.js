e943e7abfd46df47dd5105f91979c20561c34593
sebastianmorgan951 <sebastianmorgan951@gmail.com>
2021-12-22 01:37:58 -0800

    Everything before operations work! Still ugly sidebar and graphics

--- components/edge.js
+++ components/edge.js
@@ -17,8 +17,25 @@ class Edge extends React.Component {
   };
 
   render() {
-    const startCoords = this.props.startRef.current.getBoundingClientRect();
-    const endCoords = this.props.endRef.current.getBoundingClientRect();
+    let startX1;
+    let startY1;
+    let startX2;
+    let startY2;
+    if (this.props.from) {
+      /* Then an animation is going to happen, we don't want the current node
+       * position, but the last node position */
+      startX1 = this.props.from.x1;
+      startX2 = this.props.from.x2;
+      startY1 = this.props.from.y1;
+      startY2 = this.props.from.y2;
+    } else {
+      const startCoords = this.props.startRef.current.getBoundingClientRect();
+      const endCoords = this.props.endRef.current.getBoundingClientRect();
+      startX1 = `${(startCoords.left + startCoords.right) / 2}px`;
+      startX2 = `${(endCoords.left + endCoords.right) / 2}px`;
+      startY1 = `${(startCoords.top + startCoords.bottom) / 2}px`;
+      startY2 = `${(endCoords.top + endCoords.bottom) / 2}px`;
+    }
 
     return (
       <line
@@ -26,14 +43,59 @@ class Edge extends React.Component {
         id={this.props.id}
         key={this.props.id}
         ref={this.props.setRef}
-        x1={`${(startCoords.left + startCoords.right) / 2}px`}
-        y1={`${(startCoords.top + startCoords.bottom) / 2}px`}
-        x2={`${(endCoords.left + endCoords.right) / 2}px`}
-        y2={`${(endCoords.top + endCoords.bottom) / 2}px`}
+        x1={startX1}
+        y1={startY1}
+        x2={startX2}
+        y2={startY2}
         strokeWidth="10px"
         strokeLinecap="round"
         stroke="gray"
-      />
+      >
+        {this.props.to?.x1 && (
+          <animate
+            xlinkHref={`#${this.props.id}`}
+            attributeName="x1"
+            begin={this.props.begin}
+            from={startX1}
+            to={this.props.to.x1}
+            dur={this.props.dur}
+            fill="freeze"
+          />
+        )}
+        {this.props.to?.x1 && (
+          <animate
+            xlinkHref={`#${this.props.id}`}
+            attributeName="y1"
+            begin={this.props.begin}
+            from={startY1}
+            to={this.props.to.y1}
+            dur={this.props.dur}
+            fill="freeze"
+          />
+        )}
+        {this.props.to?.x2 && (
+          <animate
+            xlinkHref={`#${this.props.id}`}
+            attributeName="x2"
+            begin={this.props.begin}
+            from={startX2}
+            to={this.props.to.x2}
+            dur={this.props.dur}
+            fill="freeze"
+          />
+        )}
+        {this.props.to?.x2 && (
+          <animate
+            xlinkHref={`#${this.props.id}`}
+            attributeName="y2"
+            begin={this.props.begin}
+            from={startY2}
+            to={this.props.to.y2}
+            dur={this.props.dur}
+            fill="freeze"
+          />
+        )}
+      </line>
     );
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -71,6 +71,25 @@ const edgeReducer = (state, action) => {
         ref: action.ref,
       });
       return updated;
+    case "animations":
+      console.log("Animating edges");
+      var updated = state.map((a, i) => {
+        if (!a) return a;
+        const currAnim = action.animations[i];
+        console.log(currAnim);
+        return !currAnim
+          ? { ...a }
+          : { ...a, to: currAnim.to, from: currAnim.from };
+      });
+      return updated;
+    case "removeAnims":
+      var updated = state.map((a, i) => {
+        if (!a) return a;
+        delete a.to;
+        delete a.from;
+        return a;
+      });
+      return updated;
   }
 };
 
@@ -98,6 +117,7 @@ export default function Home() {
   const [selected, setSelected] = useState("");
   const [rootId, setRootId] = useState(0);
   const [verifyMessage, setVerifyMessage] = useState("");
+  const [isValid, setIsValid] = useState(false);
 
   let tree = [
     /*
@@ -114,6 +134,7 @@ export default function Home() {
     }
      */
   ];
+  let edgeAnimations = [];
 
   let verifyState = {
     numVisited: 0,
@@ -126,6 +147,7 @@ export default function Home() {
   };
 
   const [nodeSize, setNodeSize] = useState(50);
+  const [animationSpeed, setAnimationSpeed] = useState("500ms");
 
   const [label, labelChangeDispatch] = useReducer(labelReducer, [
     /* Contains strings corresponding to the id of node
@@ -142,6 +164,21 @@ export default function Home() {
      */
   ]);
 
+  /* Structure: Array of Objects (each object representing a node) with the
+   *             following properties
+   * {
+   *   .id: a number, the index of the node within this nodes array
+   *   .edges: an array of edge ID numbers for edges connected to this node
+   *   .ref: a reference to the currently rendered node for this object
+   *   .isBlack: whether this node is black or red in the Red-Black structure
+   * }
+   *
+   * Keeps tracks of user-created nodes, this object is used to render nodes onto
+   * the browser, we cannot resize or alter the positions of nodes within this array,
+   * as the ID of the node is actually the position of the node within this array,
+   * which allows us to quickly access nodes by id since javascript doesn't have
+   * pointers.
+   * */
   const [nodes, nodeChangeDispatch] = useReducer(nodeReducer, [
     /*{
       id: 0,
@@ -150,6 +187,24 @@ export default function Home() {
       isBlack: true,
     },*/
   ]);
+
+  /* Structure: Array of Objects (each object representing an edge) with the
+   *             following properties
+   * {
+   *   .id: a number, the index of the edge within this edges array
+   *   .start: a number, the index of one of the node endpoints of this edge,
+   *           where the index is the index of that node within the nodes arr
+   *   .end: a number, the index of the other node endpoint of this edge, where
+   *           the index is the index of that node within the nodes array
+   *   .ref: a reference to the currently rendered edge for this object
+   * }
+   *
+   * Keeps tracks of user-created edges, this object is used to render edges onto
+   * the browser, we cannot resize or alter the positions of edges within this array,
+   * as the ID of the edge is actually the position of the edge within this array,
+   * which allows us to quickly access edges by id since javascript doesn't have
+   * pointers.
+   * */
   const [edges, edgeChangeDispatch] = useReducer(edgeReducer, [
     /*{
       id: 0,
@@ -159,11 +214,19 @@ export default function Home() {
     },*/
   ]);
 
-  /** Called when use wants to enlarge space */
+  /** Called when user wants to enlarge space to create a structure
+   */
   const enlargeCanvas = () => {
     document.getElementById("canvas").style.width = "2000px";
   };
 
+  /** Called when a node is added while the structure is being created,
+   * currInd is used to keep track of the current index of the node being
+   * added to the nodes object, and sort of uses this index as a pointer
+   * to the node. We also create a ref for the current node if we need to
+   * access it at any point and perform more complicated operations on the
+   * node.
+   */
   const handleAddNode = () => {
     nodeChangeDispatch({
       type: "add",
@@ -173,8 +236,29 @@ export default function Home() {
     setCurrInd(currInd + 1);
   };
 
+  /** Called when a node is clicked, this determines what action to take based
+   * on the mouseState (the mouse setting)
+   *
+   * If "S", mouse is in Select mode, and will be used to drag the node, calling
+   *  the draggable function
+   * If "E", mouse is in Edge mode, and will be used to construct edges, first
+   *  setting the "selected" state when one node (endpoint) is clicked, then
+   *  calling the edgeLinking function when the function has two endpoints
+   * If "C", mouse is in Color mode, and will toggle the node color, switching
+   *  the color from red to black and back
+   * If "D", mouse is in Delete mode, and will call deleteNode to delete the node
+   *  in question and all connected edges
+   * If "R", mouse is in Root mode, and will set the rootId state to the current
+   *  node, changing the root of the tree structure
+   *
+   * @param e, the mousedown event which was triggered on the node
+   * @param id, the id of the node clicked on (with form node##), where ## is
+   *            the index of the node within the nodes state array
+   */
   const handleNodeClick = (e, id) => {
     const nodeIdNum = parseInt(id.match(/\d+$/)[0]);
+    nodes[nodeIdNum].ref.current.style.transition = "all 0s ease 0s";
+    setIsValid(false);
 
     switch (mouseState) {
       case "S":
@@ -202,9 +286,30 @@ export default function Home() {
     }
   };
 
+  /** This function is called to connect two nodes with an edge.
+   *
+   * This function can only be called once the "selected" state has been set to a
+   * non-empty string representing the ID of the node to be connected at one
+   * endpoint. Therefore, both endpoints of the edge are formed with the current ID
+   * parameter and the selected ID.
+   *
+   * We keep track of the current index of the edge within the edges object being
+   * added using the currEdge state object. We use this to create a sort of pointer
+   * to the edge (which is the index of the edge in the edges object). Finally, in
+   * this function, we call a few useReducer dispatch methods which adds the created
+   * edge to the array of edges associated with both endpoint nodes, and adds the
+   * edge to the edges object with the appropriate parameters, incrementing the ID
+   * of the next edge to be added, since the edges object has grown as well.
+   *
+   * @param id, the ID of the node endpoint to be connected
+   */
   const edgeLinking = (id) => {
     const endNodeNum = parseInt(id.match(/\d+$/)[0]);
     const startNodeNum = parseInt(selected.match(/\d+$/)[0]);
+
+    if (endNodeNum === startNodeNum) {
+      return;
+    }
 
     edgeChangeDispatch({
       type: "add",
@@ -228,6 +333,31 @@ export default function Home() {
     setSelected("");
   };
 
+  /** This function implements the draggable nodes.
+   *
+   * This function is called when the mouse is in select mode (mouseState === "S"),
+   * and we click on a node. Within this function, we prevent default, as when dragging,
+   * the default behavior causes the next mouseup event to be ignored for some reason.
+   *
+   * Then, we get the references to each edge which is connected to the node which has
+   * been clicked, and we record whether or not each edge is
+   *    - A self edge (an edge from the node in question to itself)
+   *    - An edge start point (The node connected to the edge.start)
+   *    - An edge end point (The node connected to the edge.end)
+   * Then, we get the position of the current node and mouse to start moving to node as
+   * we drag it around. We also set the z-Index of the node to 1000 so it moves above
+   * every other element as we are dragging it, which we set back to 10 at the end.
+   *
+   * Within this function, we add two event listeners
+   *  - For mousemove, we call the internal moveAt function, where we use the current
+   *    mouse position to update all connected edges and node positions accordingly
+   *  - For mouseup, we remove the event listeners, reset the node's z position,
+   *    and stop the draggable function
+   *
+   * @param e, the mousedown event
+   * @param id, the ID of the node clicked on, with form "node##", where ## is some
+   *            integer number representing the index of the node in the nodes array
+   */
   const draggable = (e, id) => {
     e.preventDefault();
 
@@ -244,10 +374,6 @@ export default function Home() {
      *    -1: If this node is the start of the edge
      *     0: If this edge is a self edge
      *     1: If this node is the end of the edge
-     *
-     * Array containing whether or not this node is the start or end of the edge, where
-     * the index of entry in this array corresponds to the edge at the same index of the
-     * edgeRefs array
      */
     const isStart = nodes[nodeIdNum].edges.map((a) => {
       if (edges[a].start === nodeIdNum) {
@@ -273,7 +399,6 @@ export default function Home() {
     // moves the drag at (pageX, pageY) coordinates
     // taking initial offsets into account
     function moveAt(pageX, pageY) {
-      let i = 0;
       for (let i = 0; i < edgeRefs.length; i++) {
         const edgeCase = isStart[i];
         const ref = edgeRefs[i];
@@ -313,8 +438,22 @@ export default function Home() {
     document.addEventListener("mousemove", onMouseMove);
   };
 
+  /**
+   * @param action, the object containing details of the node to be added to the tree
+   *    Action must have a few fields
+   *      .label: The label of the node, must be an integer (number)
+   *      .left: The label of the left child, must be an integer or undefined
+   *      .right: The label of the right child, must be an integer or undefined
+   *      .parent: The label of the parent node, must be an integer or undefined
+   *      .isBlack: Whether or not this node is black, must be a bool
+   *
+   * This is just a helper function to push to the tree object while we construct a
+   * more useful Red-Black Tree object to be used for future operations as we verify
+   * the structure of a user-built tree
+   */
   const addToTree = (action) => {
     tree.push({
+      nodeId: action.nodeId,
       label: action.label,
       left: action.leftLabel,
       right: action.rightLabel,
@@ -423,8 +562,6 @@ export default function Home() {
     let childEdges = [];
     for (let i = 0; i < currNode.edges.length; i++) {
       const edge = edges[currNode.edges[i]];
-
-      console.log(edge);
 
       /* Ignore self-edges */
       if (edge.start === edge.end) {
@@ -540,6 +677,7 @@ export default function Home() {
 
     /* Construct our tree */
     addToTree({
+      nodeId: currNode.id,
       label: currLabel,
       leftLabel: leftChild,
       rightLabel: rightChild,
@@ -764,9 +902,154 @@ export default function Home() {
      */
     if (isValidRedBlackTree(rootNode)) {
       setVerifyMessage("Is valid BST");
+      spaceOutTree();
+      setIsValid(true);
     }
-    console.log(verifyState);
-    console.log(tree);
+  };
+
+  const getTreeHeight = (node) => {
+    if (!node) {
+      return 0;
+    }
+    const leftHeight = getTreeHeight(tree[node.leftId]);
+    const rightHeight = getTreeHeight(tree[node.rightId]);
+
+    return 1 + (leftHeight > rightHeight ? leftHeight : rightHeight);
+  };
+
+  const spaceOutTree = () => {
+    const height = getTreeHeight(tree[0]);
+
+    /* At the bottom of a tree, a node at minimum needs space for itself, and half
+     * of its size as right padding between itself and the node on the right, and half
+     * for left padding */
+    let minimumWidthNeeded = nodeSize * 2;
+    let totalWidthNeeded = minimumWidthNeeded * Math.pow(2, height - 1);
+    let totalHeightNeeded = nodeSize + (height - 1) * 1.5 * nodeSize;
+    let windowMidpointX = window.scrollX + window.innerWidth / 2;
+    let windowMidpointY = window.scrollY + window.innerHeight / 2;
+    let windowTop = windowMidpointY - totalHeightNeeded / 2;
+
+    let widthFactor = 1.9;
+    while (totalWidthNeeded >= window.innerWidth) {
+      minimumWidthNeeded = nodeSize * widthFactor;
+      totalWidthNeeded = minimumWidthNeeded * Math.pow(2, height - 1);
+      widthFactor = widthFactor - 0.1;
+    }
+
+    edgeAnimations = new Array(edges.length).fill(null);
+
+    /* Call with (root node, position where node gets placed x, position where node
+     * gets placed y, x offset for children, y offset for children) */
+    positionTree(
+      tree[0],
+      windowMidpointX,
+      windowTop,
+      totalWidthNeeded / 4,
+      1.5 * nodeSize
+    );
+
+    edgeChangeDispatch({
+      type: "animations",
+      animations: edgeAnimations,
+    });
+    setTimeout(() => {
+      edgeChangeDispatch({
+        type: "removeAnims",
+      });
+    }, 600);
+    console.log(edgeAnimations);
+  };
+
+  const positionTree = (
+    node,
+    positionX,
+    positionY,
+    childrenOffsetX,
+    childrenOffsetY
+  ) => {
+    if (!node) {
+      return;
+    }
+
+    const nodeOffset = nodeSize / 2;
+    const renderedNodeObj = nodes[node.nodeId];
+    const currRef = renderedNodeObj.ref;
+    const currPosition = currRef.current.getBoundingClientRect();
+    const fromNodeCenterX = currPosition.left + nodeSize / 2;
+    const fromNodeCenterY = currPosition.top + nodeSize / 2;
+
+    const nodeEdges = renderedNodeObj.edges;
+
+    for (let i = 0; i < nodeEdges.length; i++) {
+      const edge = edges[nodeEdges[i]];
+      const isCurrNodeStart = edge.start === node.nodeId;
+      let from, to;
+      if (isCurrNodeStart) {
+        to = {
+          x1: positionX,
+          y1: positionY,
+        };
+        from = {
+          x1: fromNodeCenterX,
+          y1: fromNodeCenterY,
+        };
+      } else {
+        to = {
+          x2: positionX,
+          y2: positionY,
+        };
+        from = {
+          x2: fromNodeCenterX,
+          y2: fromNodeCenterY,
+        };
+      }
+      edgeAnimations[edge.id] = {
+        to: {
+          ...edgeAnimations[edge.id]?.to,
+          ...to,
+        },
+        from: {
+          ...edgeAnimations[edge.id]?.from,
+          ...from,
+        },
+      };
+      /*
+      if (edges[renderedNodeObj.edges[i]].start === node.nodeId) {
+        edge.attributes.x1.nodeValue = positionX + "px";
+        edge.attributes.x1.value = positionX + "px";
+        edge.attributes.x1.textContent = positionX + "px";
+        edge.attributes.y1.nodeValue = positionY + "px";
+        edge.attributes.y1.value = positionY + "px";
+        edge.attributes.y1.textContent = positionY + "px";
+      } else {
+        edge.attributes.x2.nodeValue = positionX + "px";
+        edge.attributes.x2.value = positionX + "px";
+        edge.attributes.x2.textContent = positionX + "px";
+        edge.attributes.y2.nodeValue = positionY + "px";
+        edge.attributes.y2.value = positionY + "px";
+        edge.attributes.y2.textContent = positionY + "px";
+      }*/
+    }
+
+    currRef.current.style.transition = "all 0.5s linear";
+    currRef.current.style.top = `${positionY - nodeOffset}px`;
+    currRef.current.style.left = `${positionX - nodeOffset}px`;
+
+    positionTree(
+      tree[node.leftId],
+      positionX - childrenOffsetX,
+      positionY + childrenOffsetY,
+      childrenOffsetX / 2,
+      childrenOffsetY
+    );
+    positionTree(
+      tree[node.rightId],
+      positionX + childrenOffsetX,
+      positionY + childrenOffsetY,
+      childrenOffsetX / 2,
+      childrenOffsetY
+    );
   };
 
   return (
@@ -844,11 +1127,18 @@ export default function Home() {
             if (!a) {
               return;
             }
+            console.log(window.performance);
+            const beginTime =
+              Date.now() - window.performance.timing.domComplete;
             return (
               <Edge
+                dur={animationSpeed}
+                to={a.to}
                 id={`edge${a.id}`}
                 key={`edge${a.id}`}
+                begin={`${beginTime}ms`}
                 setRef={a.ref}
+                from={a.from}
                 startRef={nodes[a.start].ref}
                 endRef={nodes[a.end].ref}
               />
